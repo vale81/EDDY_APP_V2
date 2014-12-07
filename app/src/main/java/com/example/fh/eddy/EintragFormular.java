18e559a6d8d5a53fa5492374521aef388c4abf57
vale81 <tim.hucke@gmail.com>
2014-12-07 20:49:35 +0100

    Layout Eintrag neu erstellt Images für Buttons hinzu Zeit und Datum jetzt TextViews mit Default statt EditText

--- app/src/main/java/com/example/fh/eddy/EintragFormular.java
+++ app/src/main/java/com/example/fh/eddy/EintragFormular.java
@@ -8,6 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
@@ -18,27 +23,45 @@ public class EintragFormular extends Activity {
     private EditText usersBz;
     private Button save_Button;
 
-
+    // Kalendar anlegen
+    private final Calendar c = Calendar.getInstance();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eintrag_formular);
         // The activity is being created.
-        // Kalender Objekt für Datum und Uhrzeit Voreinstellung
-        Calendar c = Calendar.getInstance();
-        int currentHour = c.get(Calendar.HOUR);
-        int currentMinute = c.get(Calendar.MINUTE);
-        // Voreingestellte Uhrzeit
-        EditText the_time = (EditText) findViewById(R.id.time_editText);
-        the_time.setText(currentHour + ":" + currentMinute);
-        // Voreingestelltes Datum im Datum Edittext
+        // Init Zeit und Datum TextViews
+        TextView the_date = (TextView)findViewById(R.id.date_currentDate_textView);
+        TextView the_time = (TextView) findViewById(R.id.time_currentTime_textView);
+        // Für voreingestelltes Datum im Datum Edittext
         int currentDay = c.get(Calendar.DAY_OF_MONTH);
         int currentMonth = c.get(Calendar.MONTH);
         int currentYear = c.get(Calendar.YEAR);
-        EditText the_date = (EditText)findViewById(R.id.date_editText);
         the_date.setText(currentDay + "." + currentMonth + "." + currentYear);
+        // Voreinstellung Uhrzeit, muss angepasst werden für Nullstellen
+        // 24 Std. Uhr
+        int currentHour = c.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = c.get(Calendar.MINUTE);
+        the_time.setText(nullPad(currentHour) + ":" + nullPad(currentMinute));
+
     }
+
+    //Hilfsfunktion Uhrzeit, Datum um Nullstellen richtig anzuzeigen
+    public String nullPad(int timeDateInput)
+    {
+        if(timeDateInput > 10)
+        {
+            return String.valueOf(timeDateInput);
+        }
+        else
+        {
+            return "0"+ String.valueOf(timeDateInput);
+        }
+    }
+
+
+
 
     @Override
     protected void onStart() {
