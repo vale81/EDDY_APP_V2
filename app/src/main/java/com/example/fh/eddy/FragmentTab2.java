e59d96a9ab36fd2b9cc3e250e547c6c7cc46c201
KuraishiKishimoto <fabiandominic@web.de>
2014-12-08 18:18:09 +0100

    Spinner Beispiel im Grafik Tab und Hinzufügen von default Activitys im Array.xml

--- app/src/main/java/com/example/fh/eddy/FragmentTab2.java
+++ app/src/main/java/com/example/fh/eddy/FragmentTab2.java
@@ -7,10 +7,14 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -22,14 +26,47 @@ public class FragmentTab2 extends Fragment {
         View view = inflater.inflate(R.layout.tab, container, false);
         TextView textview = (TextView) view.findViewById(R.id.tabtextview);
         //textview.setText(R.string.Grafik);
+
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+
+        fillSpinner(spinner);
+
         showUserSettings(textview);
         return view;
     }
 
+    private void fillSpinner(Spinner spinner) {
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        List<String> list = new ArrayList<String>();
+
+        Set<String> s = sharedPrefs.getStringSet("activities", new HashSet<String>());
+
+        Iterator it= s.iterator();
+        while(it.hasNext()) {
+            String item=(String)it.next();
+            list.add(item);
+        }
+
+        String[] array= getResources().getStringArray(R.array.default_activities);
+        for (String item :array) {
+            list.add(item);
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+    }
     private void showUserSettings(TextView txtview) {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
         StringBuilder builder = new StringBuilder();
+
+
 
         Set<String> s = sharedPrefs.getStringSet("activities", new HashSet<String>());
 
