3979a76a957788b576a35c3545bef8efe69fba90
KuraishiKishimoto <fabiandominic@web.de>
2014-12-22 21:27:16 +0100

    Einstellung : "Events" hinzugefügt (Ersetzt Notizen im Formular)

---
+++ app/src/main/java/com/example/fh/eddy/EventVerwaltung.java
@@ -0,0 +1,135 @@
+package com.example.fh.eddy;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Fabian on 21.11.2014.
+ */
+public class EventVerwaltung extends Activity {
+
+    SharedPreferences sharedPrefs;
+    SharedPreferences.Editor editor;
+
+    Set<String> s;
+    ListView listview;
+    ArrayList<String> list;
+    ArrayAdapter adapter;
+    //StableArrayAdapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verwaltungs_layout);
+        TextView txtview = (TextView) findViewById(R.id.textview_verwaltung);
+        txtview.setText(R.string.event_verwaltung_text);
+        Button button=(Button)findViewById(R.id.new_Item);
+        button.setText(R.string.event_verwaltung_button);
+
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPrefs.edit();
+
+        s = new HashSet<String>(sharedPrefs.getStringSet("events", new HashSet<String>()));
+        listview = (ListView) findViewById(R.id.listView);
+        list = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(this,
+                R.layout.aktivitaeten_listview_layout, R.id.firstLine, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                String text= (String)parent.getItemAtPosition(position);
+                list.remove(text);
+                s.remove(text);
+                editor.putStringSet("events",s);
+                editor.commit();
+                adapter.notifyDataSetChanged();
+                Toast toast=Toast.makeText(getApplicationContext(),getString(R.string.event)+" "+text+" "+getString(R.string.deleted), Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_HORIZONTAL,0,0);
+                toast.show();
+            }
+        });
+
+    }
+
+    public void saveItem(View view) {
+
+        EditText textfield= (EditText)findViewById(R.id.textfield_verwaltung);
+        String activity=textfield.getText().toString();
+
+        s.add(activity);
+
+        list.clear();
+        Iterator it= s.iterator();
+        while(it.hasNext()) {
+            list.add((String)it.next());
+        }
+
+        editor.putStringSet("events",s);
+        editor.commit();
+        adapter.notifyDataSetChanged();
+        Toast toast=Toast.makeText(getApplicationContext(), getString(R.string.event)+" " + activity + " "+getString(R.string.saved), Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL,0,0);
+        toast.show();
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        list.clear();
+        Iterator it= s.iterator();
+        while(it.hasNext()) {
+            list.add((String)it.next());
+        }
+
+        editor.putStringSet("events",s);
+        editor.commit();
+        adapter.notifyDataSetChanged();
+
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+}
