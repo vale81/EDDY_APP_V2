f9562f7b8dbab678c43898d6d2de215ed495a046
vale81 <tim.hucke@gmail.com>
2015-01-05 21:48:42 +0100

    Diverse Kleinigkeiten Fokus forms etc.

--- app/src/main/java/com/example/fh/eddy/SettingsFragment.java
+++ app/src/main/java/com/example/fh/eddy/SettingsFragment.java
@@ -15,6 +15,7 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
     }
 
 }
